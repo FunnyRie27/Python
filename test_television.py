df76e2b1466c9d887a271f56c4f2198ad7773613
FunnyRie27 <ms55barnes@gmail.com>
2025-04-25 16:57:43 -0500

    Create test_television.py

---
+++ test_television.py
@@ -0,0 +1,57 @@
+import pytest
+from television import *
+
+class Test:
+    def setup_method(self):
+        self.tvl = Television()
+        
+    def teardown_method(self):
+        del self.tvl
+    
+    def test_init(self):
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_power(self):
+        self.tvl.power()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        
+        self.tvl.power()
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+        
+    def test_mute(self):
+        self.tvl.power()
+        self.tvl.volume_up()
+        self.tvl.mute()
+        assert self.tvl__str__() == 'Power = True, Channel = 0, Volume = 0 '
+        
+        self.tvl.mute()
+        assert self.tvl__str__() == 'Power = True, Channel = 0, Volume = 1'
+        
+        self.tvl.mute()
+        self.tvl.power()
+        assert self.tvl__str__() == 'Power = False, Channel = 0, Volume = 1'
+        
+        self.tvl.mute()
+        assert self.tvl__str__() == 'Power = False, Channel = 0, Volume = 1'
+    
+    def test_channel_up(self):
+        self.tvl.channel_up()
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+    
+        self.tvl.power()
+        self.tvl.challen_up()
+        assert self.tvl__str__() == 'Power = True, Channel = 1, Volume = 0'
+        
+        self.tvl.channel_up()
+        self.tvl.channel_up()
+        self.tvl.channel_up()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+    def test_channel_down(self):
+        pass
+
+    def test_volume_up(self):
+        pass
+
+    def test_volume_down(self):
+        pass
