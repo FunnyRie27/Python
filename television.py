99ce975e54d1496adef676744cca0b49c7f0b37a
FunnyRie27 <ms55barnes@gmail.com>
2025-04-25 14:10:42 -0500

    Update television.py

--- television.py
+++ television.py
@@ -11,6 +11,10 @@ class Television:
         self.__channel = Television.MIN_CHANNEL
 
     def power(self):
+        """
+temp1
+
+"""
         if self.__status:
             self.__status = False
         else:
