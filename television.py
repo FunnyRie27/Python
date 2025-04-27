fcb50a8d35dcb0fcb50b274a393e96f40c067e7d
FunnyRie27 <ms55barnes@gmail.com>
2025-04-27 15:53:25 -0500

    Update television.py

--- television.py
+++ television.py
@@ -5,25 +5,26 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self) -> None:
+        """
+        Method to define variables
+        """
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
-    def power(self) -> None:
+    def power(self):
         """
         Method to toggle TV power status.
-        :return: TV ON/OFF
         """
         if self.__status:
             self.__status = False
         else:
             self.__status = True
 
-    def mute(self) -> None:
+    def mute(self):
         """
         Method to toggle TV mute status.
-        :return: Mute ON/OFF
         """
         if self.__status:
             if self.__muted:
@@ -31,42 +32,38 @@ class Television:
             else:
                 self.__muted = True
 
-    def channel_up(self) -> None:
+    def channel_up(self):
         """
         Method to toggle channel station.
-        :return: channel #>
         """
         if self.__status:
-            if self.__channel > Television.MIN_CHANNEL:
+            if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
             else:
-                self.__channel = Television.MAX_CHANNEL
+                self.__channel = Television.MIN_CHANNEL
 
-    def channel_down(self) -> None:
+    def channel_down(self):
         """
         Method to toggle channel station.
-        :return: channel <#
         """
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
             else:
-                self.__channel = Television.MIN_CHANNEL
+                self.__channel = Television.MAX_CHANNEL
 
-    def volume_up(self) -> None:
+    def volume_up(self):
         """
         Method to toggle volume level.
-        :return: volume up
         """
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self) -> None:
+    def volume_down(self):
         """
         Method to toggle volume level.
-        :return: volume down
         """
         if self.__status:
             self.__muted = False
@@ -79,6 +76,11 @@ class Television:
         :return: tv status
         """
         if self.__muted:
-            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VLOUME}'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
         else:
             return f'xxx'
+        
+        
+        
+        
+        
